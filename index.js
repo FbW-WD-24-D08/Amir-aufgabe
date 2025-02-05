8e652ce418cd523e4363aff368f7f1cc627bf601
Amirhossein Akbari <amirhossein.akbari@dci-student.org>
2025-02-05 15:46:46 +0100

    Upgrade

--- index.js
+++ index.js
@@ -10,12 +10,6 @@ const produkte = [
   { name: "Smartphone", preis: 499.99 },
   { name: "Kopfhörer", preis: 99.99 },
   { name: "Monitor", preis: 199.99 },
-  { name: "Tastatur", preis: 49.99 },
-  { name: "Maus", preis: 29.99 },
-  { name: "Drucker", preis: 149.99 },
-  { name: "Tablet", preis: 299.99 },
-  { name: "Smartwatch", preis: 199.99 },
-  { name: "Lautsprecher", preis: 79.99 },
 ];
 
 function zeigeProdukte() {
