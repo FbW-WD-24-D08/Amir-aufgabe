86519eb77d2a67f605150779d491c2f21faeb4cc
Amirhossein Akbari <amirhossein.akbari@dci-student.org>
2025-02-05 15:07:17 +0100

    Update

--- index.js
+++ index.js
@@ -24,3 +24,48 @@ function zeigeProdukte() {
     console.log(`${index + 1}. ${produkt.name} - ${produkt.preis}€`);
   });
 }
+
+readline.question(
+  "Willkommen im Online-Shop!\nWie lautet dein Name? ",
+  (name) => {
+    console.log(`\nHallo ${name}!`);
+
+    zeigeProdukte();
+
+    readline.question(
+      "\nBitte gib die Nummer des gewünschten Produkts ein: ",
+      (produktNummer) => {
+        const produktIndex = parseInt(produktNummer) - 1;
+
+        if (produktIndex >= 0 && produktIndex < produkte.length) {
+          const gewahltesProdukt = produkte[produktIndex];
+
+          readline.question(
+            `Wie viele ${gewahltesProdukt.name} möchtest du kaufen? `,
+            (anzahl) => {
+              const menge = parseInt(anzahl);
+
+              let gesamtPreis = gewahltesProdukt.preis * menge;
+
+              if (gesamtPreis > 1000) {
+                const rabatt = gesamtPreis * 0.1;
+                gesamtPreis -= rabatt;
+                console.log(`\nRabatt: 10% (${rabatt.toFixed(2)}€)`);
+              }
+
+              console.log(
+                `\n${name}, du hast ${menge}x ${gewahltesProdukt.name} bestellt.`
+              );
+              console.log(`Gesamtpreis: ${gesamtPreis.toFixed(2)}€`);
+
+              readline.close();
+            }
+          );
+        } else {
+          console.log("Fehler: Ungültige Produktauswahl!");
+          readline.close();
+        }
+      }
+    );
+  }
+);
